766fa65c8afd1c383dc851738d2bf5c7e537b72e
meichushengdexiuxia <100783361+meichushengdexiuxia@users.noreply.github.com>
2024-10-23 14:37:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git golang https://github.com/kenzok8/golang feeds/packages/lang/golang.git' >>feeds.conf.default
